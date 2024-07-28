cec152d9052307551881ab8eb83d3674363b0d0d
cookspam <cookspam.sol@gmail.com>
2024-07-28 13:02:51 -0400

    add keep alive

--- src/pages/Background/index.js
+++ src/pages/Background/index.js
@@ -356,3 +356,6 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
   sendResponse('sendResponse');
   return true;
 });
+const keepAlive = () => setInterval(chrome.runtime.getPlatformInfo, 20e3);
+chrome.runtime.onStartup.addListener(keepAlive);
+keepAlive();
